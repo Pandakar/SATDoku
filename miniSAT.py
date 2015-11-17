cd6a75e4c027345547f499d94508de86b9c82433
Myan Panikkar <myanpanikkar@gmail.com>
2015-11-17 15:18:04 -0800

    output of puzzle

--- miniSAT.py
+++ miniSAT.py
@@ -20,9 +20,28 @@ def solve_cnf_puzzle():
 	pass
 #####
 # (3) Solved Puzzle -> Readable Format
+# NOTE ----- USED 3.3 TO PRINT SECTION
+# UPDATE TO 2.7
+# INPUT: Input string is of form (abcdefghi)*9 where [a, i] are unique integers in the set [1,9]
+# This input string is essentially the same as the input string we use to solve the puzzle.
+# Main difference is that this string (should) represent the solved version of the puzzle.
+# OUTPUT: prints a 3x3 grid representation of the solved puzzle in ASCII to the console 
+#
 #####
-def output_solved_puzzle():
-	pass
+def output_solved_puzzle(solved_puzzle):
+	n = 9
+	# Split input string of a single line into 9 lines for easier printing
+	puzzle = [solved_puzzle[i:i+n] for i in range(0, len(solved_puzzle), n)]
+	n = 3
+	print("|" + "-"*7 + "+" + "-"*7 + "+" + "-"*7 + "|")
+	# Loop over all lines in puzzle and print in a nice ASCII format
+	for line in puzzle:
+		print("| " + line[0] + " " + line[1] + " " + line[2] + " | " + line[3] + " " + line[4] + " " + line[5] + " | " + line[6] + " " + line[7] + " " + line[8] + " |")
+		n -= 1
+		# Every 3 lines, print an intermediary line to split up the grid
+		if n == 0:
+			print("|" + "-"*7 + "+" + "-"*7 + "+" + "-"*7 + "|")
+			n = 3
 def main():
 	# Verify we have a puzzle (or set of puzzles) to solve
 	if len(argv) < 2:
